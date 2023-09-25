c7b9b50be386032efc45fc268aaa0056c44d6a49
clairemuiru1 <muiruclaire@gmail.com>
2023-09-25 13:38:27 +0300

    "completeted"

--- speed.js
+++ speed.js
@@ -1,13 +1,13 @@
-const speedLimit = 70;
-const demeritPoints = 0;
-if (speed < speedLimit) {
-    return  "ok"
-} else{
-   demeritPoints = math.floor((speed-speedLimit)/5)
-   return  (`points: $(demeritPoints)`) // 1 point for every 5km above 70
-   if (demeritPoints > 12){
-    return "License suspended"
-   }
-} 
+function calculateSpeed(speed){
+    let speedLimit = 70; 
+    let demeritPoints = 0;
+    if (speed < speedLimit) {
+        console.log("ok")
+    } else{
+       demeritPoints = Math.floor((speed-speedLimit)/5)
+       console.log(`Points: ${demeritPoints}`) // 1 point for every 5km above 70
+       if (demeritPoints > 12){
+        console.log("LicenseSuspended")
+       }
+    }}
     
-
--- tax.js
+++ tax.js
@@ -1,101 +1,72 @@
-let basicSalary = Number
-let benefits = Number
-let contributionBenefit = Number
-const personalRelief = 2400
-let insuraceRelief = Number
-const taxablePay = (grossSalary - (contributionBenefit + personalRelief + insuraceRelief))
-
-const grossSalary = basicSalary + benefits;
-const netSalary = grossSalary - (payee + NHIF + NSSF_I + NSSF_II);
-
-function limiter(insuraceRelief) {
-    if (insuraceRelief.value < 0) insuraceRelief.value = 0;
-    if (insuraceRelief.value > 5000) insuraceRelief.value = 5000;
-}
-function payee () {
-    switch (taxablePay) {
-        case taxablePay <= 24000:
-            return taxablePay*0.1 // 10% tax rate
-            break;
-        case taxablePay > 24001 && taxablePay<32333:
-            return taxablePay*0.25 // 25% tax rate
-            break;
-       case taxablePay >32334 && taxablePay <500000:
-           return taxablePay* 0.3 // 30% tax rate
-           break;
-       case   taxablePay >500001 &&taxablePay < 800000:
-            return taxablePay*0.325 // 32.5% tax rate 
-        default:
-            return taxablePay*0.35 // 35% tax rate for income above 800000
-            break;
+function calculateNetSalary() {
+    // Prompt the user to input basic salary and benefits
+    const basicSalary = parseFloat(prompt("Enter your basic salary:"));
+    const benefits = parseFloat(prompt("Enter your benefits:"));
+  
+    // Calculation of the gross salary
+    const grossSalary = basicSalary + benefits;
+  
+    // Calculation of the payee  
+    let payee;
+    if (grossSalary <= 24000) {
+      payee = 0.1 * grossSalary;
+    } else if (grossSalary <= 45333) {
+      payee = 3500 + 0.25 * (grossSalary - 34000);
+    } else {
+      payee = 6400 + 0.3 * (grossSalary - 43333);
     }
-}
-function NHIF() {
-    switch (grossSalary) {
-        case grossSalary < 5999:
-            return 150 // 150 is deducted
-             break;
-        case grossSalary >6000 && grossSalary<7999:
-            return 300 // 300 is deducted
-            break;
-        case grossSalary >8000 && grossSalary <11999:
-            return 400 // 400 is deducted
-            break;
-        case grossSalary >12000 && grossSalary < 14999:
-            return 500 // 500 is deducted
-            break;
-        case grossSalary >15000 && grossSalary <19999:
-            return 600 // 600 is deducted
-            break;
-        case grossSalary >20000 && grossSalary<24999:
-            return 750 // 750 is deducted
-            break;
-        case grossSalary >25000 && grossSalary< 29999:
-            return 850 // 850 is deducted
-            break;
-        case grossSalay > 30000 && grossSalary <34999:
-            return 900 // 900 is deducted
-            break;
-        case grossSalary> 35000&& grossSalary<39999: 
-            return 950 // 950 is deducted
-            break;
-        case grossSalary>40000&& grossSalary<45999:
-            return 1000 // 1000 is deducted
-            break;
-        case grossSalary> 45000&& grossSalary<49999:
-            return 1100 // 1100 is deducted
-            break;
-        case grossSalary> 50000&& grossSalary< 59999:
-            return 1200 //1200 is deducted
-            break;
-        case grossSalary>60000 && grossSalary< 69999:
-            return 1300 // 1300 is deducted
-            break;
-        case grossSalary> 70000 && grossSalary<79999:
-            return 1400 // 1400 is deducted
-            break;
-        case grossSalary> 80000&& grossSalary<89999:
-            return 1500 // 1500 is deducted
-            break;
-        case grossSalary> 90000&& grossSalary<99999:
-            return 1600 // 1600 is deducted
-            break;
-        default:
-            return 1700 // 1700 is deducted for grossSalary above 100000
-            break;
+  
+    // Calculate NHIF deductions
+    let nhifDeductions;
+    if (grossSalary <= 5999) {
+      nhifDeductions = 150;
+    } else if (grossSalary >= 6000 && grossSalary <= 7999) {
+      nhifDeductions = 300;
+    } else if (grossSalary >= 8000 && grossSalary <= 11999) {
+      nhifDeductions = 400;
+    } else if (grossSalary >= 12000 && grossSalary <= 14999) {
+      nhifDeductions = 500;
+    } else if (grossSalary >= 15000 && grossSalary <= 19999) {
+      nhifDeductions = 600;
+    } else if (grossSalary >= 20000 && grossSalary <= 24999) {
+      nhifDeductions = 750;
+    } else if (grossSalary >= 25000 && grossSalary <= 29999) {
+      nhifDeductions = 850;
+    } else if (grossSalary >= 30000 && grossSalary <= 34999) {
+      nhifDeductions = 900;
+    } else if (grossSalary >= 35000 && grossSalary <= 39999) {
+      nhifDeductions = 950;
+    } else if (grossSalary >= 40000 && grossSalary <= 44999) {
+      nhifDeductions = 1000;
+    } else if (grossSalary >= 45000 && grossSalary <= 49999) {
+      nhifDeductions = 1100;
+    } else if (grossSalary >= 50000 && grossSalary <= 59999) {
+      nhifDeductions = 1200;
+    } else if (grossSalary >= 60000 && grossSalary <= 69999) {
+      nhifDeductions = 1300;
+    } else if (grossSalary >= 70000 && grossSalary <= 79999) {
+      nhifDeductions = 1400;
+    } else if (grossSalary >= 80000 && grossSalary <= 89999) {
+      nhifDeductions = 1500;
+    } else if (grossSalary >= 90000 && grossSalary <= 99999) {
+      nhifDeductions = 1600;
+    } else if (grossSalary >= 100000) {
+      nhifDeductions = 1700;
     }
-}
-function NSSF_I() {
-    function limiter(NSSF_I) {
-        if (NSSF_I.value < 0) NSSF_I.value = 0;
-        if (NSSF_I.value > 5000) NSSF_I.value = 6000;
-        return (grossSalary * 0.06) // 6% of pensionable pay to NSSF
-    }
-}
-function NSSF_II() {
-    function limiter(NSSF_II) {
-        if (NSSF_II.value < 0) NSSF_II.value = 6001;
-        if (NSSF_II.value > 5000) NSSF_II.value = 18000;
-        return (grossSalary * 0.06) // 6% of pensionable pay to NSSF
-    }
-}
+  
+    // Calculation of NSSF deductions 
+    const nssfDeductions = 0.06 * grossSalary;
+  
+    // Calculation of net salary
+    const netSalary = grossSalary - payee - nhifDeductions - nssfDeductions;
+  
+    // Display the calculated values 
+    alert(`Gross Salary: $${grossSalary.toFixed(2)}`);
+    alert(`Payee (Tax): $${payee.toFixed(2)}`);
+    alert(`NHIF Deductions: $${nhifDeductions.toFixed(2)}`);
+    alert(`NSSF Deductions: $${nssfDeductions.toFixed(2)}`);
+    alert(`Net Salary: $${netSalary.toFixed(2)}`);
+  }
+  
+  // Call the function for calculation and display the salary details
+  calculateNetSalary();
