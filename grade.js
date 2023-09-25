056a68a10329f22d2a8c215b75ea91924f005cbb
clairemuiru1 <muiruclaire@gmail.com>
2023-09-25 13:14:52 +0300

    completed work

--- grade.js
+++ grade.js
@@ -1,19 +1,19 @@
 function studentGradeGeneator(){
     switch (grade) {
     case grade >79:
-       grade = A;
+       grade = A; // anything above 79
         break;
     case   grade >= 60 && grade <= 79:
-       grade = B;
+       grade = B; // between 60 and 79
        break;
     case grade >= 49 && grade <= 59:
-        grade = C;
+        grade = C; // between 49 and 59
         break;
     case grade >=  40 && grade <= 49:
-        grade = D;
+        grade = D; // between 40 and 49
         break;
     case grade > 40:
-        grade = E;
+        grade = E; // anything below 40
         break;      
     
 }}
--- speed.js
+++ speed.js
@@ -4,7 +4,7 @@ if (speed < speedLimit) {
     return  "ok"
 } else{
    demeritPoints = math.floor((speed-speedLimit)/5)
-   return  (`points: $(demeritPoints)`)
+   return  (`points: $(demeritPoints)`) // 1 point for every 5km above 70
    if (demeritPoints > 12){
     return "License suspended"
    }
